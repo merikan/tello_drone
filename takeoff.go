89052fca5ed1a6946b743187b1544cd428724f54
Peter Merikan <peter@merikan.com>
2018-05-01 21:30:29 +0200

    initial commit

---
+++ takeoff.go
@@ -0,0 +1,42 @@
+// +build example
+//
+// Do not build by default.
+
+/*
+You must have ffmpeg and OpenCV installed in order to run this code. It will connect to the Tello
+and then open a window using OpenCV showing the streaming video.
+	https://github.com/hybridgroup/gocv
+	go get -u -d gocv.io/x/gocv
+	brew install opencv
+
+
+	go run opencv.go
+*/
+package main
+
+import (
+	"time"
+
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/platforms/dji/tello"
+)
+
+func main() {
+	drone := tello.NewDriver("8888")
+
+	work := func() {
+		drone.TakeOff()
+
+		gobot.After(5*time.Second, func() {
+			drone.Land()
+		})
+	}
+
+	robot := gobot.NewRobot("tello",
+		[]gobot.Connection{},
+		[]gobot.Device{drone},
+		work,
+	)
+
+	robot.Start()
+}
---
+++ video.go
@@ -0,0 +1,59 @@
+// +build example
+
+/*
+You must have ffmpeg and OpenCV installed in order to run this code. It will connect to the Tello
+and then open a window using OpenCV showing the streaming video.
+	https://github.com/hybridgroup/gocv
+	go get -u -d gocv.io/x/gocv
+	brew install opencv
+
+
+	go run opencv.go
+*/
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"time"
+
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/platforms/dji/tello"
+)
+
+func main() {
+	drone := tello.NewDriver("8890")
+
+	work := func() {
+		mplayer := exec.Command("mplayer", "-fps", "25", "-")
+		mplayerIn, _ := mplayer.StdinPipe()
+		if err := mplayer.Start(); err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		drone.On(tello.ConnectedEvent, func(data interface{}) {
+			fmt.Println("Connected")
+			drone.StartVideo()
+			drone.SetVideoEncoderRate(tello.VideoBitRateAuto)
+			gobot.Every(100*time.Millisecond, func() {
+				drone.StartVideo()
+			})
+		})
+
+		drone.On(tello.VideoFrameEvent, func(data interface{}) {
+			pkt := data.([]byte)
+			if _, err := mplayerIn.Write(pkt); err != nil {
+				fmt.Println(err)
+			}
+		})
+	}
+
+	robot := gobot.NewRobot("tello",
+		[]gobot.Connection{},
+		[]gobot.Device{drone},
+		work,
+	)
+
+	robot.Start()
+}
